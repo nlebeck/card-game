09987829892479bfc97f00e9e4237c0bce4ae903
Niel Lebeck <niel.lebeck@gmail.com>
2016-09-13 20:16:50 -0700

    Add simple UWP client that talks to Jetty server

---
+++ client/UWPClient/UWPClient/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPClient
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            string responseText = "No response";
+            HttpClient client = new HttpClient();
+            HttpRequestMessage request = new HttpRequestMessage();
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(urlTextBox.Text);
+                responseText = await response.Content.ReadAsStringAsync();
+            }
+            catch(Exception exception)
+            {
+                responseText = "Error: " + exception;
+            }
+
+            this.responseTextBlock.Text = responseText;
+        }
+    }
+}
