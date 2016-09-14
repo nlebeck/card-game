2d2bb7a84e56658eac9e84dd23ca89c7ee0aa910
Niel Lebeck <niel.lebeck@gmail.com>
2016-09-13 19:33:14 -0700

    Add Jetty to project and create a simple Jetty server

--- server/CardGameServer/src/main/java/niellebeck/cardgameserver/CardGameServer.java
+++ server/CardGameServer/src/main/java/niellebeck/cardgameserver/CardGameServer.java
@@ -1,13 +1,30 @@
 package niellebeck.cardgameserver;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+import niellebeck.cardgameserver.handlers.DefaultHandler;
+
 /**
  * The server for the card game
  *
  */
-public class CardGameServer 
+public class CardGameServer extends AbstractHandler
 {
+    private static final int PORT = 8080;
+    
     public static void main( String[] args )
     {
+    	Server server = new Server(PORT);
+    	server.setHandler(new DefaultHandler());
     	
+    	try {
+    	    server.start();
+    	    server.join();
+    	}
+    	catch (Exception e) {
+    	    System.err.println("Error starting server: " + e.getMessage());
+    	    System.exit(1);
+    	}
     }
 }
