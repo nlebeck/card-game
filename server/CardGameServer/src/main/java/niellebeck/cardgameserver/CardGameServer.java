bb89c79b2f0811b40c5f9c2facf0d4eaff4bee60
Niel Lebeck <niel.lebeck@gmail.com>
2016-09-13 19:06:12 -0700

    Create Maven project for server

---
+++ server/CardGameServer/src/main/java/niellebeck/cardgameserver/CardGameServer.java
@@ -0,0 +1,13 @@
+package niellebeck.cardgameserver;
+
+/**
+ * The server for the card game
+ *
+ */
+public class CardGameServer 
+{
+    public static void main( String[] args )
+    {
+    	
+    }
+}
